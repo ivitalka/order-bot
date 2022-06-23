447c8559ae51eaa8f59397ec023810b16468510e
IVitalka <ivitalka24@gmail.com>
2022-06-23 21:10:28 +0700

    initial commit

--- Scenes.js
+++ Scenes.js
@@ -1,26 +1,26 @@
 const { Scenes } = require('telegraf')
 require('dotenv').config({path: __dirname + '/.env'});
-const {validatePhone, validateOrder, result} = require('./Utils')
-
+const {validatePhone, validateOrder} = require('./Utils')
+let result = {}
 
 class SceneGenerator {
     GenPhoneScene () {
         const phone = new Scenes.BaseScene('phone')
         phone.enter(async (ctx) => {
-            await ctx.reply('Введите номер телефона.')
+            await ctx.reply('Введите номер вашего телефона')
         })
         phone.on('text', async (ctx) => {
             if(validatePhone(ctx.message.text)){
-                await ctx.reply('Успешно')
+                await ctx.reply('Успешно!')
                 result.phone = ctx.message.text
                 await ctx.scene.enter('order')
             }
             else {
-                await ctx.reply('Не корректно введен номер.')
+                await ctx.reply('Некорректно введен номер телефона')
                 await ctx.scene.reenter('phone')
             }
         })
-        phone.on('message', (ctx) => ctx.reply('Введите номер телефона!'))
+        phone.on('message', (ctx) => ctx.reply('Введите номер вашего телефона'))
         return phone
     }
 
@@ -28,16 +28,16 @@ class SceneGenerator {
     GenOrderScene () {
         const order = new Scenes.BaseScene('order')
         order.enter(async (ctx) => {
-            await ctx.reply('Введите номер заявки')
+            await ctx.reply('Введите номер вашей заявки из смс(номер заявки содержит 13 цифр)')
         })
         order.on('text', async (ctx) => {
             if(validateOrder(ctx.message.text)){
-                await ctx.reply('Номер заявки добавлен успешно')
+                await ctx.reply('Номер заявки добавлен успешно!')
                 result.order = ctx.message.text
                 await ctx.scene.enter('time')
             }
             else {
-                await ctx.reply('Не корректный номер заявки')
+                await ctx.reply('Некорректный номер заявки. Введите номер заявки из смс, номер заявки содержит 13 цифр')
                 await ctx.scene.reenter('order')
             }
         })
@@ -51,10 +51,12 @@ class SceneGenerator {
             await ctx.reply('Введите удобное время для звонка оператора.')
         })
         time.on('text', async (ctx) => {
-                await ctx.reply('Время установлено успешно.')
-                await ctx.reply('Ваша заявка принята, ожидайте звонка оператора.')
+                await ctx.reply('Время установлено успешно!')
+                await ctx.reply('Ваша заявка доставлена, наш оператор свяжется с Вами в указанное время. Спасибо, за обращение!')
                 result.time = ctx.message.text
-                await ctx.telegram.sendMessage(process.env.CHAT_ID, result)
+                await ctx.telegram.sendMessage(process.env.CHAT_ID,
+                `Телефон: ${result.phone} \nЗаявка № ${result.order} \nВремя для перезвона: ${result.time}`)
+                result = {}
                 await ctx.scene.leave()
         })
         time.on('message', (ctx) => ctx.reply('Укажите удобное время для звонка оператора!'))
--- Utils.js
+++ Utils.js
@@ -3,16 +3,10 @@ function validatePhone(phone){
     return regex.test(phone);
 }
 
-function validateName(name) {
-    let regex = /^[a-zA-Zа-яА-Я'][a-zA-Zа-яА-Я-' ]+[a-zA-Zа-яА-Я']?$/u
-    return regex.test(name)
-}
-
 function validateOrder(order) {
     let regex = /^\d{13,}$/u
     return regex.test(order)
 }
 
-const result = {}
 
-module.exports = {validatePhone, validateName, validateOrder, result};
+module.exports = {validatePhone, validateOrder};
