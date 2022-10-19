f03424c00ba1e7cd3652b820d63828c8456af254
IVitalka <ivitalka24@gmail.com>
2022-10-19 18:48:53 +0300

    Добавил функционал рассылки рекламы

--- Utils/AdvertVariables.js
+++ Utils/AdvertVariables.js
@@ -1,5 +1,6 @@
 const videoAdFilter = {'adReceived.videoAd' : false}
-const videoAdImageId = 'AgACAgIAAxkBAAILRWNHHcHip-1LFaX6FWWG4H4fm4s_AAIWxDEbn5Q5SlP42A5TN6DZAQADAgADcwADKgQ'
+// const videoAdImageId = 'AgACAgIAAxkBAAILRWNHHcHip-1LFaX6FWWG4H4fm4s_AAIWxDEbn5Q5SlP42A5TN6DZAQADAgADcwADKgQ'
+const videoAdImageId = 'https://monosnap.com/file/w8iBY0QHcgnwEus6CMYhnMGf26t6H4'
 const videoAdMessage = '<b>Приобретайте <a href="https://msk.rt.ru/videocontrol?utm_source=telegram&utm_medium=messenger&utm_campaign=b2c_videocontrol_tgb">оборудование для Умного дома</a>, в рассрочку всего от 390р, и присматривайте за домом пока отсутствуете!</b>' +
     '\n\nС помощью HD камеры и удобного приложения «Умный дом» на вашем смартфоне,' +
     ' Вы сможете просматривать онлайн трансляции или записи с камеры видеонаблюдения' +
