4de2a03ca5d0396102ffd7c9b97772f82aabc0e3
IVitalka <ivitalka24@gmail.com>
2022-10-19 13:41:53 +0300

    Добавил функционал рассылки рекламы

--- index.js
+++ index.js
@@ -54,6 +54,46 @@ bot.action('btn_2', async (ctx) => {
 <a href="https://drive.google.com/file/d/1avU8Sf3SM2kCsiBH2uRt0qcryFjCBLFm">Политикой конфиденциальности и Пользовательским соглашением</a>`)
 })
 
+bot.action('btn_videoAd', async (ctx) => {
+    await UserModel.findOne({userId: ctx.from.id})
+        .then(async (user) => {
+            if(!user) {
+                return ctx.reply('Вы не зарегистрированы в системе!')
+            } else {
+                await CounterModel.find({'adReceived.videoAd' : false})
+                    .then(async (users) => {
+                        const totalSend = users.length
+                        let successSend = 0
+                        let blockedSend = 0
+                        for(let i = 0; i < users.length; i++){
+                            let user = users[i].userId
+                            await bot.telegram.sendPhoto(user, 'AgACAgIAAxkBAAILRWNHHcHip-1LFaX6FWWG4H4fm4s_AAIWxDEbn5Q5SlP42A5TN6DZAQADAgADcwADKgQ')
+                                .catch(err => console.log(err))
+                            await bot.telegram.sendMessage(user,
+                                '<b>Приобретайте <a href="https://msk.rt.ru/videocontrol?utm_source=telegram&utm_medium=messenger&utm_campaign=b2c_videocontrol_tgb">оборудование для Умного дома</a>, в рассрочку всего от 390р, и присматривайте за домом пока отсутствуете!</b>' +
+                                '\n\nС помощью HD камеры и удобного приложения «Умный дом» на вашем смартфоне,' +
+                                ' Вы сможете просматривать онлайн трансляции или записи с камеры видеонаблюдения' +
+                                ' и оперативно реагировать на непредвиденные события, происходящие в Вашем доме.', {parse_mode: 'HTML'})
+                                .then(async (res) => {
+                                    if (res.message_id){
+                                        successSend++
+                                        await CounterModel.updateOne(
+                                            {userId: user},
+                                            {$set: {'adReceived.videoAd': true}})
+                                            .catch(err => console.log(err))
+                                    }
+                                })
+                                .catch((err) => {
+                                    if (err.response.error_code === 403) blockedSend++
+                                })
+                        }
+                        ctx.reply(`Всего отправлено: ${totalSend} \nНе доставлено: ${blockedSend} \nДоставлено: ${successSend}`)
+                    })
+                    .catch(err => console.log(err))
+            }
+        })
+})
+
 bot.command('order_call', async (ctx) => {
         await ctx.scene.enter('phone')
 })
@@ -84,38 +124,38 @@ bot.command('update',  async (ctx) => {
         })
 })
 
-bot.command('find', async (ctx) => {
-    await CounterModel.find({'adReceived.videoAd' : false})
-        .then(async (users) => {
-            const totalSend = users.length
-            let successSend = 0
-            let blockedSend = 0
-            for(let i = 0; i < users.length; i++){
-                let user = users[i].userId
-                await bot.telegram.sendPhoto(user, 'AgACAgIAAxkBAAILRWNHHcHip-1LFaX6FWWG4H4fm4s_AAIWxDEbn5Q5SlP42A5TN6DZAQADAgADcwADKgQ')
-                    .catch(err => console.log(err))
-                await bot.telegram.sendMessage(user,
-                    '<b>Приобретайте <a href="https://msk.rt.ru/videocontrol?utm_source=telegram&utm_medium=messenger&utm_campaign=b2c_videocontrol_tgb">оборудование для Умного дома</a>, в рассрочку всего от 390р, и присматривайте за домом пока отсутствуете!</b>' +
-                    '\n\nС помощью HD камеры и удобного приложения «Умный дом» на вашем смартфоне,' +
-                    ' Вы сможете просматривать онлайн трансляции или записи с камеры видеонаблюдения' +
-                    ' и оперативно реагировать на непредвиденные события, происходящие в Вашем доме.', {parse_mode: 'HTML'})
-                    .then(async (res) => {
-                        if (res.message_id){
-                            successSend++
-                            await CounterModel.updateOne(
-                                {userId: user},
-                                {$set: {'adReceived.videoAd': true}})
-                                .catch(err => console.log(err))
-                        }
-                    })
-                    .catch((err) => {
-                        if (err.response.error_code === 403) blockedSend++
-                    })
-            }
-            ctx.reply(`Всего отправлено: ${totalSend} \nНе доставлено: ${blockedSend} \nДоставлено: ${successSend}`)
-        })
-        .catch(err => console.log(err))
-})
+// bot.command('find', async (ctx) => {
+//     await CounterModel.find({'adReceived.videoAd' : false})
+//         .then(async (users) => {
+//             const totalSend = users.length
+//             let successSend = 0
+//             let blockedSend = 0
+//             for(let i = 0; i < users.length; i++){
+//                 let user = users[i].userId
+//                 await bot.telegram.sendPhoto(user, 'AgACAgIAAxkBAAILRWNHHcHip-1LFaX6FWWG4H4fm4s_AAIWxDEbn5Q5SlP42A5TN6DZAQADAgADcwADKgQ')
+//                     .catch(err => console.log(err))
+//                 await bot.telegram.sendMessage(user,
+//                     '<b>Приобретайте <a href="https://msk.rt.ru/videocontrol?utm_source=telegram&utm_medium=messenger&utm_campaign=b2c_videocontrol_tgb">оборудование для Умного дома</a>, в рассрочку всего от 390р, и присматривайте за домом пока отсутствуете!</b>' +
+//                     '\n\nС помощью HD камеры и удобного приложения «Умный дом» на вашем смартфоне,' +
+//                     ' Вы сможете просматривать онлайн трансляции или записи с камеры видеонаблюдения' +
+//                     ' и оперативно реагировать на непредвиденные события, происходящие в Вашем доме.', {parse_mode: 'HTML'})
+//                     .then(async (res) => {
+//                         if (res.message_id){
+//                             successSend++
+//                             await CounterModel.updateOne(
+//                                 {userId: user},
+//                                 {$set: {'adReceived.videoAd': true}})
+//                                 .catch(err => console.log(err))
+//                         }
+//                     })
+//                     .catch((err) => {
+//                         if (err.response.error_code === 403) blockedSend++
+//                     })
+//             }
+//             ctx.reply(`Всего отправлено: ${totalSend} \nНе доставлено: ${blockedSend} \nДоставлено: ${successSend}`)
+//         })
+//         .catch(err => console.log(err))
+// })
 
 bot.command('advert', async (ctx) => {
     await UserModel.findOne({userId: ctx.message.from.id})
